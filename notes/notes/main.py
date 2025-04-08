ca5e9a4f0e80465d9a88e80091b482e8bbb4c990
willemmirkovich <willemmirkovich@gmail.com>
2025-04-08 15:36:37 -0400

    working meeting notes

--- notes/notes/main.py
+++ notes/notes/main.py
@@ -1,14 +1,53 @@
-from os import getenv, path
+from os import getenv, path, mkdir
 from argparse import ArgumentParser, Namespace
+from jinja2 import Environment, FileSystemLoader
+from datetime import datetime
+from typing import Tuple
 
 
 WORK_DIR = getenv('NOTES_WORK_DIR')
+TEMPLATE_DIR = path.abspath(path.join(__file__, '..', 'templates'))
 
+
+env = Environment(
+    loader=FileSystemLoader(TEMPLATE_DIR)
+)
+
+
+def _normalize_name(name: str):
+    return name.strip().lower().replace(' ', '_')
+
+
+def _current_timpestamp() -> Tuple[str, str]:
+    dt = datetime.now()
+    date = dt.strftime('%Y%m%d')
+    timestamp = dt.strftime('%b %d, %Y %H:%M')
+    return date, timestamp 
 
 
 def meeting(args: Namespace):
-    pass
+    """
+    Meeting note handler
+    """
+    dir = path.join(WORK_DIR, 'meetings')
+    if not path.exists(dir):
+        mkdir(dir)
 
+    try:
+        name = args.name
+    except AttributeError:
+        raise ValueError(f'meeting template requires `name` argument')
+    
+    template = env.get_template('meeting.jinja.md')
+
+    date, ts = _current_timpestamp()
+    clean_name = _normalize_name(name)
+    file_name = f'{clean_name}_{date}.md'
+    file_path = path.join(dir, file_name)
+
+    with open(file_path, 'w') as f:
+        data = template.render(name=name, timestamp=ts)
+        f.write(data)
 
 def main():
 
@@ -22,6 +61,7 @@ def main():
     parser.add_argument('template')
 
     # meeting template
+    parser.add_argument('--name')
 
     # daily log template
 
---
+++ notes/notes/templates/meeting.jinja.md
@@ -0,0 +1,19 @@
+# {{ name }}
+
+When: `{{ timestamp }}`
+
+## Objective
+
+[ TODO ]
+
+## Attendees
+
+[ TODO ]
+
+## Notes
+
+- something
+
+## Action Items
+
+- [ ] item 1
